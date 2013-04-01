8e37aca6613436f697775cf4160a059ca84a12bf
mar Jinn <marjinn@live.com>
2013-04-01 05:33:10 +0530

    pending

---
+++ notifierElite/performanceOptimization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace notifierElite
+{
+    class performanceOptimization
+    {
+        public performanceOptimization()
+        { }
+
+        public void clearRecycleBin()
+        {
+
+        }
+
+
+
+    }
+}
