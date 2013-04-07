3c8e5c172d15375f9cced94c6451c4ea43ba6a49
mar Jinn <marjinn@live.com>
2013-04-07 06:20:36 +0530

    webService

--- notifierElite/ConsumeWebService.cs
+++ notifierElite/ConsumeWebService.cs
@@ -1,23 +1,26 @@
-﻿using System;
+﻿
+#region usingDirectives
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 using notifierElite.smb_WebService_Reference;
+#endregion
 
 namespace notifierElite
 {
     class ConsumeWebService
     {
+        ///Instantiate the Webservice
+        smb_WebService_Reference.SMBService _SMBService
+            = new smb_WebService_Reference.SMBService();
         public ConsumeWebService()
         {
-            ///Instantiate the Webservice
-            smb_WebService_Reference.SMBService _SMBService 
-                = new smb_WebService_Reference.SMBService();
         }
 
         public void Check_Agent()
         {
-
+            _SMBService.CheckAgentAsync("thangappa", "solve123#");
         }
     }
 }
